78b596ee977afab510634bd9b83b1792655c21d5
Natsuki <ibron1975@gmail.com>
2025-01-21 01:53:50 +0900

    mobile-nav:blog

--- app/components/mobile-nav.tsx
+++ app/components/mobile-nav.tsx
@@ -11,7 +11,7 @@ import {
 
 const tags = [
   { label: "Home", href: "/" },
-  // { label: "About", href: "/about" },
+  { label: "Blog", href: "/blog" },
   { label: "Ai", href: "/ai" },
   { label: "Contact", href: "/contact" },
 ]
